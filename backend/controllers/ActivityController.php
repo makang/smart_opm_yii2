79548ad198a06fdfd2c30f93a4000629cdcdfc82
chenyitian <chenyitian@wepiao.com>
2016-06-13 13:55:43 +0800

    活动列表页

--- backend/controllers/ActivityController.php
+++ backend/controllers/ActivityController.php
@@ -1,12 +1,92 @@
 <?php
-
+/**
+ * @date  2016-1-4
+ * @description 老活动列表
+ * @author cyt
+ * @copyright 2016
+ * @since 1.0
+ */
 namespace backend\controllers;
+use backend\models\SmartPriceCut;
+use Yii;
+use backend\controllers\CommonController;
+use backend\models\OpenBasePublicsignal;
+use backend\models\Article;
+use backend\models\ArticleSearch;
 
-class ActivityController extends \yii\web\Controller
-{
-    public function actionList()
-    {
-        return $this->render('list');
+class ActivityController  extends CommonController{
+
+
+    public function init(){
+    	parent::init();
+    	// 检查操作员是否有对本功能操作的权限
+    	
     }
 
+ 	public function actionList(){
+        $searchModel = new SmartPriceCut();
+        $dataProvider   = $searchModel->oSearch(Yii::$app->request->queryParams);
+        $activityStatus = $searchModel::$_STATUS_SHOW;
+        return $this->render('list', [
+            'dataStatus'   => $activityStatus,
+            'dataProvider'  => $dataProvider
+        ]);
+    }
+
+
+    /*
+    * 删除活动列表
+    *
+    */
+    public function actionDelete(){
+        $params     =   $this->sGetUrlParam(Yii::$app->request->getReferrer());
+        $jumpUrl    =   'activity/list?'.$params;
+        $pc_id = Yii::$app->request->get('id');
+        if(intval($pc_id)){
+            $res = SmartPriceCut::model()->bDelActivity($pc_id);
+            if($res)$this->jump($jumpUrl);
+        }else{
+            $this->jump($jumpUrl);
+        }
+    }
+
+
+    /*
+    * 开启活动
+    *
+    */
+    public function actionStart(){
+        $params     =   $this->sGetUrlParam(Yii::$app->request->getReferrer());
+        $jumpUrl    =   'activity/list?'.$params;
+        $pc_id = Yii::$app->request->get('id');
+        if(intval($pc_id)){
+            $res = SmartPriceCut::model()->bStartActivity($pc_id);
+            if($res)$this->jump($jumpUrl);
+        }else{
+            $this->jump($jumpUrl);
+        }
+    }
+
+    /*
+    * 暂停活动
+    *
+    */
+    public function actionStop(){
+        $params     =   $this->sGetUrlParam(Yii::$app->request->getReferrer());
+        $jumpUrl    =   'activity/list?'.$params;
+        $pc_id = Yii::$app->request->get('id');
+        if(intval($pc_id)){
+            $res = SmartPriceCut::model()->bStopActivity($pc_id);
+            if($res)$this->jump($jumpUrl);
+
+        }else{
+            $this->jump($jumpUrl);
+        }
+    }
+
+
+
 }
+ 
+ 
+ ?>
--- backend/controllers/CommonController.php
+++ backend/controllers/CommonController.php
@@ -1,13 +1,32 @@
 <?php
 
 namespace backend\controllers;
-
+use Yii;
 class CommonController extends \yii\web\Controller
 {
     public function init(){
         parent::init();
         // 检查操作员是否有对本功能操作的权限
 
+    }
+
+    /**跳转
+     * @param $url
+     * @param $withParam    是否带get参数跳转
+     */
+    protected  function jump($url,$withParam=''){
+        if($withParam){
+            foreach($withParam as $k=>$v){
+                $get[] = $k.'='.$v;
+            }
+            $url .= '?'.implode('&',$get);
+        }
+        $this->redirect( Yii::$app->getUrlManager()->createUrl($url));
+    }
+
+    protected function sGetUrlParam($url){
+        $url = parse_url($url);
+        return $url['query'];
     }
 
 }
