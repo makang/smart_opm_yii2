510ff2b4b707e03f2b13887b1970f4c1ec8542d8
刘奎 <290378120@qq.com>
2016-06-14 15:17:37 +0800

    修改公告功能

--- backend/controllers/NoticeController.php
+++ backend/controllers/NoticeController.php
@@ -3,16 +3,35 @@
 namespace backend\controllers;
 
 use app\models\OpmOpSystemNotice;
+use app\models\OpmOpSystemNoticeTags;
+use Yii;
+use app\models\OpmOpSystemNoticeSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
 
 class NoticeController extends \yii\web\Controller
 {
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
     public function actionList()
     {
-        $model=new OpmOpSystemNotice();
-        echo \Yii::t('app', 'Hello, {username}!', [
-            'username' => 'Alexander',
-        ]);exit();
-        return $this->render('lists',['model'=>$model]);
+        $searchModel = new OpmOpSystemNoticeSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
     }
 
 }
