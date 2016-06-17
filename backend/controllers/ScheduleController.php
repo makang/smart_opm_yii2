84db89ac21e43a57b676a0a22ed3e24f17005e76
刘奎 <290378120@qq.com>
2016-06-17 09:51:48 +0800

    停售排期功能完成

--- backend/controllers/ScheduleController.php
+++ backend/controllers/ScheduleController.php
@@ -2,11 +2,23 @@
 
 namespace backend\controllers;
 
+use backend\models\SmartSchedule;
+
 class ScheduleController extends \yii\web\Controller
 {
     public function actionQuery()
-    {
+    { 
         return $this->render('query');
     }
+    public function actionStatistics(){
+        $statisticInfo=SmartSchedule::getStatistic();
 
+       return $this->render('statistics',['schedule_list'=>$statisticInfo['schedule_list'],
+           'total_num'=>$statisticInfo['total_num'],
+           'stop_num'=>$statisticInfo['stop_num'],
+           'top_schedule_list'=>$statisticInfo['top_schedule_list']]);
+    }
+    public function actionSchedulequery(){
+
+    }
 }
