041da0724ea01be4ded05f1628827c552019f471
chenyitian <chenyitian@wepiao.com>
2016-06-21 17:00:43 +0800

    公众号管理

--- backend/models/SmartPublicSignal.php
+++ backend/models/SmartPublicSignal.php
@@ -18,7 +18,7 @@ class SmartPublicSignal extends \yii\db\ActiveRecord
 {
     public static $_STATUS_COORPING = 1;         //合作中
     public static $_STATUS_NOCOOR   = 0;         //合作停止
-
+    //公众号类型
     public static $_THEME_DESC = array(
         '1'  =>  '影院',
         '2'  =>  '院线',
@@ -26,10 +26,26 @@ class SmartPublicSignal extends \yii\db\ActiveRecord
         '4'  =>  '影视服务公司',
 
     );
+    //合作状态
     public static $_COOR_DESC = array(
         '1'  =>  '合作中',
         '2'  =>  '已结束',
 
+    );
+    //公众号类型
+    public static $_TYPE_DESC = array(
+        '1' =>  '服务号',
+        '2' =>  '订阅号',
+    );
+    //是否受理商
+    public static $_ACCEPT_DESC = array(
+        0   =>  '否',
+        1   =>  '是'
+    );
+    //是否子账号
+    public static $_CACCOUNT_DESC   = array(
+        0   =>  '否',
+        1   =>  '是'
     );
 
 
@@ -49,14 +65,26 @@ class SmartPublicSignal extends \yii\db\ActiveRecord
         return $this::findOne(['pid'=>$pid]);
     }
 
-
+    //获取合作状态
     public function sGetCoorStatus($status){
         if(!array_key_exists($status,self::$_COOR_DESC)){
-            return '未开始';
+            return '已结束';
         }else{
             return self::$_COOR_DESC[$status];
         }
     }
+
+    /**根据pid停止合作
+     * @param $pid
+     * @return bool
+     */
+    public function bUpdateStopCoor($pid){
+        $res = $this::find()->where(['pid'=>$pid])->one();
+        $res->CooperationStatus = self::$_STATUS_NOCOOR;
+        return $res->save();
+
+    }
+    //获取合作主题文字
     public function sGetThemeDesc($id){
         return self::$_THEME_DESC[$id];
     }
