847fea3cddd713e4fb02045c7f9a488fac916992
chenyitian <chenyitian@wepiao.com>
2016-06-23 11:04:12 +0800

    新的布局

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,6 +1,38 @@
 <?php
 
-/* @var $this yii\web\View */
-
-$this->title = '智慧影院后台管理系统';
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+use backend\models\Article;
+use backend\models\SmartCinema;
+use yii\widgets\ActiveForm;
 ?>
+<div class="row">
+    <div class="col-xs-12">
+
+
+
+
+
+    </div><!-- /span -->
+</div><!-- /row -->
+
+
+<script type="text/javascript">
+    window.jQuery || document.write("<script src='/assets_ace/js/jquery-2.0.3.min.js'>"+"<"+"/script>");
+</script>
+<script src="/assets_ace/js/jquery-ui-1.10.3.full.min.js"></script>
+
+
+<script type="text/javascript">
+    jQuery(function($) {
+    });
+
+
+
+</script>
+
+
+
+
+
