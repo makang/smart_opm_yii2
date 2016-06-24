c00d60ec65088470038e0753a283af8b16b428a0
刘奎 <290378120@qq.com>
2016-06-24 10:44:09 +0800

    修改排期查询的样式。

--- backend/views/schedule/schedule.php
+++ backend/views/schedule/schedule.php
@@ -4,19 +4,8 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\ActiveForm;
 ?>
-<style>
-    .control-group{
-        width:100%;
-        height:60px;
-        background-color: #26ffe1;
-    }
-    .control-label {
-        float: left;
-        padding-top: 5px;
-        text-align: center;
-        width: 260px;
-    }
-</style>
+
+<script src='/assets_ace/js/jquery-2.0.3.min.js'></script>
 <div class="control-group">
         <label class="control-label"> 影院名称：<font color="#FF0000"><?php echo isset($cinema_name)?$cinema_name:'';?></font></label>
 			<span class="fl">
@@ -44,7 +33,7 @@ use yii\widgets\ActiveForm;
                 ?>><?php echo  Html::a($movie,
                             ['query-schedule', 'cinema_no' => $cinema_no,'cinema_name' => $cinema_name,'movie_name' => $movie],
                             [
-                                'class' => 'btn btn-xs btn-info',
+                                'class' => '',
                             ]
                         );?>
                     </dd>
@@ -187,12 +176,7 @@ use yii\widgets\ActiveForm;
                                         default:
                                             echo '未知系统';
                                             break;
-                                    }
-
-
-
-
-                                    ?></td>
+                                    }?></td>
                             </tr>
                         <?php }}else{}?>
                     </tbody>
@@ -202,10 +186,25 @@ use yii\widgets\ActiveForm;
     <?php }?>
 </div>
 <!-- 右侧内容 end -->
-<script>
+<script type="text/javascript">
     var len=<?php echo count($show_date); ?>;
+
+//    setTimeout(function () {
+//        for(var i=0;i<len;i++){
+//            console.log("test:"+i);
+//            if(i==0){
+//                $('#div'+i).show();
+//                $('#dataclass'+i).attr('style','background-color:green');
+//            }else{
+//                $('#div'+i).hide();
+//                $('#dataclass'+i).attr('style','');
+//            }
+//        }
+//    },200);
+
     $(document).ready(function(){
         for(var i=0;i<len;i++){
+            console.log("test:"+i);
             if(i==0){
                 $('#div'+i).show();
                 $('#dataclass'+i).attr('style','background-color:green');
@@ -229,3 +228,154 @@ use yii\widgets\ActiveForm;
     }
 
 </script>
+<style>
+    .control-group{
+        width:100%;
+        height:60px;
+        background-color: rgba(38, 255, 225, 0);
+    }
+    .control-label {
+        float: left;
+        padding-top: 5px;
+        text-align: center;
+        width: 260px;
+    }
+    .sy-list-nav ul li{
+        float: left;
+        display: block;
+        min-width: 100px;
+        padding:0px 10px;
+        height: 39px;
+        text-align: center;
+        background:#f9f9f9;
+        margin-right: 5px;
+        line-height: 38px;
+        cursor: pointer;
+        border-top: 1px solid #707070;
+        border-left: 1px solid #707070;
+        border-right: 1px solid #707070;
+        border-top-right-radius: 5px;
+        border-top-left-radius: 5px;
+    }
+
+    .x_aside {
+        width: 20%;
+        float: left;
+        background: #fff;
+        z-index: 2;
+        position: relative;
+        display: block;
+        min-height: 100px;
+
+    }
+    .contents {
+        width: 1198px;
+        margin: 0 auto;
+    }
+
+
+    .x_asideDIV {
+    }
+    .x-menu-l {
+        font-size: 16px;
+        color: #515151;
+        margin: 0;
+    }
+
+    .x-menu-l dt, .x-menu-l dd {
+        height: 40px;
+        border-left: 1px solid #9c9c9c;
+        border-right: 1px solid #9c9c9c;
+    }
+
+    .x-menu-l dt {
+        background: #f3f3f3;
+    }
+
+    .x-menu-l dt span, .x-menu-l dd span {
+        display: block;
+        border-top: 1px solid #fff;
+        height: 38px;
+        line-height: 38px;
+    }
+
+    .x-menu-l dt span {
+        padding-left: 20px;
+        border-bottom: 1px solid #9c9c9c;
+        background: url(../images/x-icn.png) no-repeat right 0px;
+    }
+
+    .x-menu-l dd {
+        background-color: #fff;
+        cursor: pointer;
+        margin: 0
+    }
+
+    .x-menu-l dd span {
+        padding-left: 25px;
+        font-size: 12px;
+        border-bottom: 1px solid #9c9c9c;
+    }
+
+    .x-menu-l dd.cur {
+        border-left: 3px solid #6ad000;
+        border-right: 1px solid #fff;
+        background-color: #fff;
+        margin: 0
+    }
+
+    .x-menu-l dd.cur span {
+        padding-left: 23px;
+    }
+    .x-CustomerInformation {
+        text-align: left;
+        font-size: 14px;
+        padding:40px 10px;
+        color: #333;
+        border-bottom: 1px solid #9c9c9c;
+        border-right: 1px solid #9c9c9c;
+        border-left: 1px solid #9c9c9c;
+        line-height: 20px;
+        ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½word-wrap:break-word;
+    }
+    .x-CustomerInformation span{
+        font-size: 16px;
+        font-weight: bold;
+    }
+    .x_aside {
+        width: 20%;
+        float: left;
+        background: #fff;
+        z-index: 2;
+        position: relative;
+        display: block;
+        min-height: 100px;
+
+    }
+
+    .btn-success {
+        color: #ffffff;
+        text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);
+        background-color: #5bb75b;
+        *background-color: #51a351;
+        background-image: -moz-linear-gradient(top, #62c462, #51a351);
+        background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#62c462), to(#51a351));
+        background-image: -webkit-linear-gradient(top, #62c462, #51a351);
+        background-image: -o-linear-gradient(top, #62c462, #51a351);
+        background-image: linear-gradient(to bottom, #62c462, #51a351);
+        background-repeat: repeat-x;
+        border-color: #51a351 #51a351 #387038;
+        border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);
+        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ff62c462', endColorstr='#ff51a351', GradientType=0);
+        filter: progid:DXImageTransform.Microsoft.gradient(enabled=false);
+    }
+
+    .right-contents {
+        width: 79%;
+        /*padding: 20px;*/
+        border-left: 1px solid #9c9c9c;
+        float: left;
+        /*margin-left: -1px;*/
+        min-height: 400px;
+    }
+</style>
