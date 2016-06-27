dffac93c3798f76cf99a667c66f8a4361d30f433
刘奎 <290378120@qq.com>
2016-06-27 14:03:16 +0800

    订单查询增加日期限制

--- backend/views/order/activity.php
+++ backend/views/order/activity.php
@@ -24,13 +24,13 @@ $this->params['breadcrumbs'][] = $this->title;
                             <i class="icon-calendar"></i>
                             <input type="text" id="datepickers_start" name="start_date" class="search-query datetimepicker"
                                    placeholder="开始日期"
-                                   value="<?= !empty($_REQUEST['start_date']) ? $_REQUEST['start_date'] : ""; ?>"/>
+                                   value="<?= !empty($_REQUEST['start_date']) ? $_REQUEST['start_date'] : date('Y-m-d H:i:s',time()-2*24*60*60); ?>"/>
                         </span>
                          <span class="input-icon align-middle">
                             <i class="icon-calendar"></i>
                             <input type="text" id="datepickers_end" name="end_date" class="search-query datetimepicker"
                                    placeholder="结束日期"
-                                   value="<?= !empty($_REQUEST['end_date']) ? $_REQUEST['end_date'] : ""; ?>"/>
+                                   value="<?= !empty($_REQUEST['end_date']) ? $_REQUEST['end_date'] : date('Y-m-d H:i:s',time()); ?>"/>
                         </span>
                         <span class="input-icon align-middle">
                             <i class="icon-search"></i>
